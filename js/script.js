b1079544bee6ca95f4612315e85a5822965380e5
basheer421 <bammar@student.42abudhabi.ae>
2023-07-12 20:37:41 +0400

    logo responsive with theme

--- js/script.js
+++ js/script.js
@@ -34,25 +34,27 @@ $("#hex").on("click", function () {
 
 // Light/Dark toggle
 const checkbox = document.getElementById('checkbox');
+const logo = document.querySelector('.logo');
 
 function checkDarkMode(){
   if (localStorage.getItem("tourism_website_darkmode") !== null && localStorage.getItem("tourism_website_darkmode") === "true") {
     document.body.classList.add('dark');
+    logo.classList.add('dark');
     checkbox.checked = true;
   }
 };
+
 checkDarkMode();
 
 checkbox.addEventListener('change', () => {
   document.body.classList.toggle('dark');
+  logo.classList.toggle('dark');
   document.body.classList.contains('dark') ? 
     localStorage.setItem("tourism_website_darkmode", true) :
     localStorage.setItem("tourism_website_darkmode", false); 
 });
 
-
 // scroll button
-
 let mybutton = document.getElementById("upbtn");
 
 window.onscroll = function() {scrollFunction()};
