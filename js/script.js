aa2938ee1c623a11556dfe2d4a54aa5c384a38d7
basheer laptop <basheer753@gmail.com>
2023-07-15 16:55:42 +0400

    added whatsapp and removed s_media links

--- js/script.js
+++ js/script.js
@@ -70,3 +70,39 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
 }
+
+var url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?66623';
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = url;
+var options = {
+"enabled":true,
+"chatButtonSetting":{
+  "backgroundColor":"#00e785",
+  "ctaText":"Chat with us",
+  "borderRadius":"25",
+  "marginLeft": "30",
+  "marginRight": "20",
+  "marginBottom": "20",
+  "ctaIconWATI":false,
+  "position":"left"
+},
+"brandSetting":{
+  "brandName":"socotraescape",
+  "brandSubTitle":"undefined",
+  "brandImg":"https://socotratime.com/img/logo.png",
+  "welcomeText":"Hi there!\nHow can I help you?",
+  "messageText":"",
+  "backgroundColor":"#00e785",
+  "ctaText":"Chat with us",
+  "borderRadius":"25",
+  "autoShow":false,
+  "phoneNumber":"971526954048"
+}
+};
+s.onload = function() {
+  CreateWhatsappChatWidget(options);
+};
+var x = document.getElementsByTagName('script')[0];
+x.parentNode.insertBefore(s, x);
